9c456011df0a7c7be9ec34b3d07c71a9bc198c15
Jefferson Faustin <faustinj@hotmail.fr>
2020-05-06 18:40:26 -0400

    first commit

---
+++ src/Components/Quote.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import 'react-bootstrap-carousel/dist/bootstrap.min.css';
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
+import {React_Bootstrap_Carousel} from 'react-bootstrap-carousel';
+
+
+class Quotes extends Component {
+    constructor() {
+        super();
+        this.state = {
+            quote: "",
+            author: ""
+        }
+    }
+
+
+
+
+
+    render() {
+        return {
+            <div>
+            </div>
+            
+        }
+    }
+}
